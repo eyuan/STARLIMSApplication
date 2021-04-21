aaa9c895e20d9a493c4c25ea2cb2184ed67ce67a
马洪彪 <e_yuan@foxmail.com>
2021-04-21 10:42:02 +0800

    1

---
+++ LimsApplication/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace LimsApplication
+{
+    partial class FmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tspb = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bgw
+            // 
+            this.bgw.WorkerReportsProgress = true;
+            this.bgw.WorkerSupportsCancellation = true;
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw_ProgressChanged);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(37, 31);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(165, 43);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "导出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssl,
+            this.tspb});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 122);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(429, 25);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssl
+            // 
+            this.tssl.Name = "tssl";
+            this.tssl.Size = new System.Drawing.Size(84, 20);
+            this.tssl.Text = "消息提示：";
+            // 
+            // tspb
+            // 
+            this.tspb.Name = "tspb";
+            this.tspb.Size = new System.Drawing.Size(300, 19);
+            // 
+            // FmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 147);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnExport);
+            this.Name = "FmMain";
+            this.Text = "应用程序代码导出工具";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssl;
+        private System.Windows.Forms.ToolStripProgressBar tspb;
+    }
+}
+
---
+++ LimsApplication/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LimsApplication
+{
+    public partial class FmMain : Form
+    {
+        public FmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
